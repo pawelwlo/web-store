b2f85f12a7149fb78d3e6c17622a0dabfb975ce7
Pawel <113646840+pawelwlo@users.noreply.github.com>
2023-02-24 05:18:08 +0100

    "minor changes"

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,44 +1,43 @@
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-
-
-export default function Cart({product, cartItems, removeItem}) {
-
-    function deleteFromCart() {
-    
-    if(product.cartItems !== 0) {
-    product.setCartItems(product.cartItems - 1)
+export default function Cart({ product, cartItems, removeItem, cartCounter }) {
+  function deleteFromCart() {
+    if (product.cartItems !== 0) {
+      product.setCartItems(product.cartItems - 1);
     }
-}
-    return (
-        <div>
-            <h2>Cart</h2>
-            <ul>
-                {(cartItems.length > 0) ? 
+  }
 
-                    cartItems.map((item,index) => 
-                    
-                        <li className="product-card-cart" key={index} >
-                            
-                         <img className='product-img-cart' src={item.image}/>
-                        <p className='product-title'> {item.title}</p>
-                        <p className="price">{item.price} $</p> 
-                        <div></div>  
-                        <button className='removeButton' onClick={() => removeItem(item)}>remove</button>
-                        </li>
-                        
-                        ):
+  return (
+    <div className='cart'>
+      <div className='cart-top'>
+        <h2>Cart</h2>
+        <span>{cartCounter}</span>
+        <img className='img-cart' src='./cart.png' alt='cart icon' />
+      </div>
 
-                    <div>
-                no items in your cart
-                    </div>
-            }
-            
-            </ul>
-            
+      {cartItems.length > 0 ? (
+        <ul>
+          {cartItems.map((item, index) => (
+            <li className='product-card-cart' key={index}>
+              <img className='product-img-cart' src={item.image} alt={item.title} />
+              <p className='product-title'>{item.title}</p>
+              <p className='price'>{item.price} $</p>
+              <div></div>
+              <button className='removeButton' onClick={() => removeItem(item)}>
+                remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className='cart-bottom'>
+          <p>no items in your cart</p>
+          <p className='cart-link'>
+            <NavLink to={'/'}>back to shopping</NavLink>
+          </p>
         </div>
-        
-    )
+      )}
+    </div>
+  );
 }
